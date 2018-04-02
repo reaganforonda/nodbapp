bdec0c09b47900fec283cda0364bfb8bc464da03
reaganforonda <foronda.reagan@gmail.com>
2018-04-02 09:02:43 -0600

    fixed input field to reset after submit

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -117,6 +117,7 @@ export default class Display extends Component {
         <div className="search-section">
           <input
             className="search-input"
+            value = {this.state.location}
             placeholder="Enter Zip Code"
             onChange={e => this.handleInputChange(e)}
           />
