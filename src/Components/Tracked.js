cf64a84f8b14541bcc2bff631274ee2bcb1939c6
reaganforonda <foronda.reagan@gmail.com>
2018-04-02 11:20:54 -0600

    refractored the buttons in the Tracked componeent

--- src/Components/Tracked.js
+++ src/Components/Tracked.js
@@ -14,44 +14,22 @@ function Tracked(props) {
           <p>{`Temperature: ${val.temp}`} &#8457;</p>
           <div className="weather-item-buttons">
 
-            {/* REfractored */}
-            <button
-              className="Weather-item-button"
-              onClick={() => props.delete(val.id)}
-            >
-              Remove
-            </button>
-
-            <button
-              className="Weather-item-button"
-              onClick={() => props.move(val.id, "up")}
-            >
-              Move Up
-            </button>
-
-                  
-            <button
-              className="Weather-item-button"
-              onClick={() => props.move(val.id, "down")}
-            >
-              Move Down
-            </button>
-
-            {/* <Button
+            <Button
               cssClass={"Weather-item-button"}
               onclick={() => props.delete(val.id)}
-              title={"Remove-test"}
+              title={"REMOVE"}
             />
             <Button
               cssClass={"Weather-item-button"}
               onclick={() => props.move(val.id, "up")}
-              title={"Move up-test"}
+              title={"MOVE UP"}
             />
-            {/* <Button
+
+            <Button
               cssClass={"Weather-item-button"}
               onclick={() => props.move(val.id, "down")}
-              title={"Move Down-test"}
-            /> */}
+              title={"MOVE DOWN"}
+            />
           </div>
         </div>
       </div>
